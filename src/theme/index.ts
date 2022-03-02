be2806a09ccd4cdf3c41d5df501bc57ca43453b7
jeziel <jezielmarques7@gmail.com>
2022-03-01 21:42:45 -0300

    create and typed app theme

---
+++ src/theme/index.ts
@@ -0,0 +1,19 @@
+export default {
+  COLORS: {
+    TEXT: '#3E64A6',
+    YELLOW: '#F7F40A',
+    RED_900: '#D40404',
+    RED_800: '#D93243',
+    BACKGROUND: '#f2f2f2',
+
+    PRIMARY_900: '#058CB3',
+    PRIMARY_800: '#2BBCDE',
+    PRIMARY_700: '#12CBEC',
+  },
+
+  FONTS: {
+    REGULAR: 'Montserrat_400Regular',
+    MEDIUM: 'Montserrat_500Medium',
+    BOLD: 'Montserrat_700Bold',
+  },
+}
